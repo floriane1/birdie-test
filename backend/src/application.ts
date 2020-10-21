c75ef0df422e1e086a6b2c054004c6901986bc0d
floriane1 <florianedidier@gmail.com>
2020-10-21 12:45:59 -0300

    enable CORS

--- backend/src/application.ts
+++ backend/src/application.ts
@@ -1,9 +1,11 @@
 import * as express from "express";
+import * as cors from "cors";
 import { pingController } from "./controllers/ping";
 import { careRecipientsController } from "./controllers/careRecipients";
 
 const app = express();
 
+app.use(cors());
 app.use(pingController);
 app.use(careRecipientsController);
 
