6ecb0638b4590cb3a64cea36e75e906ba4f80168
floriane1 <florianedidier@gmail.com>
2020-10-22 23:00:51 -0300

    compression middleware added

--- backend/src/application.ts
+++ backend/src/application.ts
@@ -1,12 +1,14 @@
 import * as express from "express";
 import * as cors from "cors";
 import * as path from "path";
+import * as compression from "compression";
 import { pingController } from "./controllers/ping";
 import { careRecipientsController } from "./controllers/careRecipients";
 
 const app = express();
 
 app.use(cors());
+app.use(compression());
 app.use(pingController);
 app.use(careRecipientsController);
 app.use(express.static(path.join(__dirname, "../../front-end/build")));
